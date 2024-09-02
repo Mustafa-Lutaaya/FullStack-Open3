055425af262bab23c26d373fea2d840775638c7a
Mustafa-Lutaaya <cestmoimuganga@gmx.de>
2024-09-02 08:48:32 +0200

    Exercise 3.2 - Phonebook Backend Step 3

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.get('/api/persons',(request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  const numberofRecords = persons.length;
+  const requestTime = new Date();
+
+  response.send(`
+    <p>Phonebook has info for ${numberofRecords} people </p>
+    <p>${requestTime}</p>
+    `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
