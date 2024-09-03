65c3a4050e5bdfdec8cf0605b8cb8052bd6edee7
Mustafa-Lutaaya <cestmoimuganga@gmx.de>
2024-09-04 01:03:25 +0200

    Exercise 3.19 - Phonebook Database Step 7

--- models/person.js
+++ models/person.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose')
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minlength: [3, 'Name must be atleast 3 characters long'],
+        required: true
+    },
+    number: {
+        type: String,
+        required: true
+    }
 });
+
+personSchema.set('runValidators', true);
 
 module.exports = mongoose.model('Person', personSchema);
